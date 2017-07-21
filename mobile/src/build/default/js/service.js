51694bab87d03979d9e317b05823eaf8969be944
othmaan <aaothmaan@gmail.com>
2017-07-21 13:46:51 +0400

    - Add android project + updates in structure

--- mobile/src/build/default/js/service.js
+++ mobile/src/build/default/js/service.js
@@ -3,28 +3,32 @@
     Ref. https://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
 */
 
-let PhotoStore = function () {
-    
+function GameStore() {
+
     //////// Private ////////
     let _cache = [];
-
+    let url = 'http://localhost:1337';
 
     //////// Interface ////////
     return {
-        getRandomPhotos: getRandomPhotos
+        getRandomPhotos: _getRandomPhotos,
+        createNewUser: _createUser,
+        getUserDetails: _getUser,
+        addScoreToUser: _createScore,
+        getTopScore: _getTopScore
     }
 
 
     //////// Implementation ////////
     /* Returns number of random photos */
-    function getRandomPhotos(number) {
+    function _getRandomPhotos(number) {
         // 1.
         return new Promise((resolve, reject) => {
             _get().then(photos => {
                 let all = [];
                 let rand = Math.floor(Math.random() * (photos.length - number));
                 for (i = 0; i < number; i++) {
-                    all.push(photos[rand + i]); 
+                    all.push(photos[rand + i]);
                 }
                 resolve(all);
             }).catch(error => {
@@ -46,16 +50,88 @@ let PhotoStore = function () {
                         'Content-Type': 'application/json'
                     }
                 };
-                fetch('https://api.500px.com/v1/photos/search?term=kitten&feature=popular&only=11&include_store=0' + 
-                '&tags=0&include_states=0&image_size=3&consumer_key=UpwVHaShpniIM00n77hIPTsjWc8tLV4RfTay9PqJ', params)
+                fetch('https://api.500px.com/v1/photos/search?term=kitten&feature=popular&only=11&include_store=0' +
+                    '&tags=0&include_states=0&image_size=3&consumer_key=UpwVHaShpniIM00n77hIPTsjWc8tLV4RfTay9PqJ', params)
+                    .then(_checkStatus)
+                    .then(_parseJSON)
+                    .then(json => {
+                        resolve(json.photos);
+                    }).catch(e => {
+                        reject(e);
+                    });
+            }
+        });
+    }
+
+    /* Add user to db - Should take user object, for simplicity takes name only */
+    function _createUser(name) {
+        return new Promise((resolve, reject) => {
+            let params = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'name': name
+                })
+            };
+            fetch(url + '/user', params)
                 .then(_checkStatus)
                 .then(_parseJSON)
                 .then(json => {
-                    resolve(json.photos);
+                    resolve(json);
                 }).catch(e => {
                     reject(e);
                 });
-            }
+        });
+    }
+
+    function _getUser() {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/user' + '?sort=score%20DESC')
+                .then(_checkStatus)
+                .then(_parseJSON)
+                .then(json => {
+                    resolve(json);
+                }).catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    function _createScore(id, score) {
+         return new Promise((resolve, reject) => {
+            let params = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'owner': id,
+                    'score': score
+                })
+            };
+            fetch(url + '/score', params)
+                .then(_checkStatus)
+                .then(_parseJSON)
+                .then(json => {
+                    resolve(json);
+                }).catch(e => {
+                    reject(e);
+                });
+        });
+    }
+
+    function _getTopScore() {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/score' + '?sort=score%20DESC')
+                .then(_checkStatus)
+                .then(_parseJSON)
+                .then(json => {
+                    resolve(json);
+                }).catch(e => {
+                    reject(e);
+                });
         });
     }
 
@@ -75,3 +151,48 @@ let PhotoStore = function () {
         return res.json();
     }
 }
+
+/* Generate random names & other stuff */
+function Utils() {
+
+    //////// Private ////////
+    const adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
+    const nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnomes", "glitter", "potato", "salad", "toejam", "curtains", "beets", "toilet", "exorcism", "stick figures", "mermaid eggs", "sea barnacles", "dragons", "jellybeans", "snakes", "dolls", "bushes", "cookies", "apples", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot air balloon", "praying mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman"];
+
+    //////// Interface ////////
+    return {
+        pickRandomName: _pickName
+    }
+
+    //////// Implementation ////////
+    function _pickName() {
+        var first = adjectives[Math.floor(Math.random() * adjectives.length)];
+        var last = nouns[Math.floor(Math.random() * nouns.length)];
+        return first.toUpperCase() + " " + last.toUpperCase();
+    }
+}
+
+/* User class */
+class User {
+    constructor(id, name, scores) {
+        this.id = id ? id : 0;
+        this.name = name ? name : '';
+        this.scores = scores ? scores : [];
+    }
+
+    addGameScore(score) {
+        this.scores.push(score);
+    }
+
+    /* Returns to scores in descending order */
+    getTopScores() {
+        // TBD
+    }
+
+    /* Returns recent scores */ 
+    getRecentScores() {
+        // TBD
+    }
+}
+/* [[ARCHITECUTRE]]: More absctaction is needed. Make Scores class that has id, timestamp, score
+*/
--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,88 +1,197 @@
 const app = {
-    initialize: function () {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-    onDeviceReady: function () {
-        if ('addEventListener' in document) {
-
-            /* Fastclick polyfill */
-            document.addEventListener('DOMContentLoaded', () => {
-                FastClick.attach(document.body);
-            }, false);
-
-            /* Remove grey highlight in iOS */
-            document.addEventListener("touchstart", () => {}, true);
-        }
-    }
+    initialize: _initialize,
+    onDeviceReady: _deviceReady
 };
 
-// 0. App initialize 
 app.initialize();
+
 
 // 1. Reference elements
 let gameScore = document.querySelector('game-score');
 let gameCards = document.querySelectorAll('game-card');
-let store = new PhotoStore();
-let cards = [];
+let allCards = document.querySelector('#all-cards');
+let currentUser = new User();
+let store = new GameStore();
+let utils = new Utils();
+var cards = [];
+var trials = 0;
+gameScore.hidden = true;
 
-// 2. Get high score
-gameScore.score = 1000;
+// 2. Create new user
+store.createNewUser(utils.pickRandomName()).then(user => {
+    currentUser.id = user.id;
+    currentUser.name = user.name;
+});
 
-// 3. Get 3 random images
-store.getRandomPhotos(3).then(photos => {
+// 3. Get high score
+store.getTopScore().then(json => {
+    // 3.1 Filter to get number
 
-    gameCards.forEach(el => {
-        let random = Math.floor(Math.random() * photos.length);
-        el.url = photos[random].image_url;
-        el.id = photos[random].id;
-    }, this);
+    // 3.2 Update UI
+    gameScore.score = 1000;
+    gameScore.items = [{name:'aa', score: 2000}];
+
 }).catch(e => {
     console.error(e);
 });
 
-// 4. Implement game logic
+
+// 4. Get random images & update cards
+store.getRandomPhotos(3).then(photos => {
+
+    // 4.1 Get random list idx 
+    let idx = _randomIdx(6);
+
+    // 4.2 Get list of photos
+    let allPhotos = photos.concat(photos);
+
+    // 4.3 Update each element's id & url
+    idx.forEach(i => {
+        gameCards[i].url = allPhotos[i].image_url;
+        gameCards[i].id = allPhotos[i].id;
+    }, this);
+
+}).catch(e => {
+    console.error(e);
+});
+
+// 4. Called when user clicks a card
 function onReveal(id, index) {
-    // 1.
+    // 4.1
+    ++trials;
+
+    // 4.2
     gameCards.forEach(el => {
-        // 1.1 Only look for revealed & enabled cards
+        // 4.2.1 Only look for revealed & enabled cards
         if (!el.unrevealed && !el.disable) {
-            
-            // 1.2 Make sure to push unique cards that isn't already existing
-            if(!cards.find(c => {return c.idx === el.idx})) {
-                
-                // 1.3 
+
+            // 4.2.2 Make sure to push unique cards that isn't already existing
+            if (!cards.find(c => { return c.idx === el.idx })) {
+
+                // 4.2.3 
                 cards.push(el);
             }
         }
     }, this);
-    
-    // 2. 
+
+    // 4.3
     setTimeout(() => {
-        
-        // 2.1 
+
+        // 4.3.1 
         if (cards.length >= 2) {
-            
-            // 2.2 To escape user selecting the same card twice
+
+            // 4.3.2 To escape user selecting the same card twice
             if (cards[0].id === cards[1].id && cards[0].idx !== cards[1].idx) {
                 cards[0].disable = true;
                 cards[1].disable = true;
+                cards = [];
             } else {
-                
-                // 2.3 Unreveal all cards if no matches are found
+
+                // 4.3.3 Unreveal all cards if no matches are found
                 cards.forEach(c => {
-                    c.unrevealed = true;
+                    c.unrevealed = !c.unrevealed;
+                    c.classList.toggle("ff");
                 });
+                cards = [];
             }
-            cards = [];
         }
 
-        // 2.4 Add small delay for usability
+        var allDisabled = [];
+        for(el of gameCards) {
+            if (el.disable) { allDisabled.push(el) }
+        }
+        if(allDisabled.length == 6) {
+            _finishGame();
+        }
+
+        // 4.3.4 Add small delay for usability
     }, 1500);
 
 }
+
+
+/* Scoring system */
+function _calculateScore(trials) {
+    // Best scenario is  (26 - 6) * 500 = 10,000. Worst scenario (26 - 26) * 500 = 0.
+    return (26 - trials) * 500;
+}
+
+/* Finish game */
+function _finishGame() {
+    // 1. Calculate score
+    var score = _calculateScore(trials);
+
+    // 2. Update ViewModel local & remote
+    currentUser.addGameScore(score);
+    store.addScoreToUser(currentUser.id, score);
+
+    // 3. Update UI
+    gameScore.hidden = false;
+    allCards.hidden = true;
+    gameScore.name = currentUser.name;
+    gameScore.score = currentUser.scores[0];
+    trials = 0;
+}
+/* [[ARCHITECUTRE]]: After submitting score, make method to update user ViewModel. 
+
+2 options:
+  - Call getUserById() 'TBD'
+  - Push changes in ViewModel after success to server. Order depends if offline first apporach is prefered or not.
+
+If game is offline first (I support that) a network queue must be created to manage request/response/success/failure cycle.
+*/
+
+
+/* Restart game */
+function _restartGame() {
+    // TBD
+}
+
+/* Generate random Idx */
+function _randomIdx(n) {
+    let arr = Array.apply(null, { length: n }).map(Number.call, Number);
+    let current = arr.length, temp, random;
+
+    while (0 !== current) {
+        // Pick a remaining element...
+        random = Math.floor(Math.random() * current);
+        current -= 1;
+
+        // And swap it with the current element.
+        temp = arr[current];
+        arr[current] = arr[random];
+        arr[random] = temp;
+    }
+
+    return arr;
+}
+
+
+/* App initialize */
+function _initialize() {
+    document.addEventListener('deviceready', app.onDeviceReady.bind(this), false);
+}
+
+/* Device ready event listener */
+function _deviceReady() {
+    if ('addEventListener' in document) {
+
+        /* Fastclick polyfill */
+        document.addEventListener('DOMContentLoaded', () => {
+            FastClick.attach(document.body);
+        }, false);
+
+        /* Remove grey highlight in iOS */
+        document.addEventListener("touchstart", () => { }, true);
+    }
+}
+
+
 
 /*
 References
 https://css-tricks.com/snippets/css/remove-gray-highlight-when-tapping-links-in-mobile-safari/
 https://github.com/ftlabs/fastclick
+https://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 */
